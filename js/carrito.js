992880b2bdb81f8eb9ef8601c7a9ca9dbbe35810
AbelContti <conttiabel@outlook.com>
2024-12-12 14:19:20 -0300

    Cambios finales
    Cambios en color.
    Agrego botón y funcionalidad para eliminar producto en carrito.
    Ajustes en archivo carrito.js.
    Ajustes en algunos estilos de la página.

--- js/carrito.js
+++ js/carrito.js
@@ -15,21 +15,23 @@ function cargarCarrito(){
                     <img src="./imgs/productos/${producto.id}.jpg" alt="${producto.id}">
                     <p>${producto.titulo}</p>
                 </div>
-                    
-                <p>Cantidad: ${producto.cantidad}</p>
-                    
+                
                 <div class="botones">
-                    <button id="btn_agregar">+</button> 
+                <button id="btn_agregar">+</button> 
+                    <p style="display:inline-block;">${producto.cantidad}</p>
                     <button id="btn_restar">-</button>
                 </div>
 
                 <p class="subtotal">Subtotal: $${producto.cantidad * producto.precio}</p>
+
+                <span class="material-symbols-outlined icono_eliminar">delete</span>
             `;
             
             totalCompra += producto.cantidad * producto.precio;
             
             divDetalle.querySelector("#btn_agregar").addEventListener("click", () => sumarProducto(carrito, producto));
             divDetalle.querySelector("#btn_restar").addEventListener("click", () => restarProducto(carrito, producto));
+            divDetalle.querySelector(".icono_eliminar").addEventListener("click", () => eliminarProducto(carrito, producto));
             divCarrito.appendChild(divDetalle);
         }
 
@@ -46,32 +48,49 @@ function cargarCarrito(){
     
 }
 
+function actualizarLocalStorage(carrito, cantidadProductos){
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    localStorage.setItem("cantidadProductos", JSON.stringify(cantidadProductos));
+}
+
 function sumarProducto(carrito, producto){
     let cantidadProductos = Number (localStorage.getItem("cantidadProductos"));
     
     producto.cantidad++;
     cantidadProductos++;
     
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-    localStorage.setItem("cantidadProductos", JSON.stringify(cantidadProductos));
+    // localStorage.setItem("carrito", JSON.stringify(carrito));
+    // localStorage.setItem("cantidadProductos", JSON.stringify(cantidadProductos));
     
+    actualizarLocalStorage(carrito, cantidadProductos);
     actualizarCarrito();
     cargarCarrito();
 }
 
 function restarProducto(carrito, producto){
     let cantidadProductos = Number (localStorage.getItem("cantidadProductos"));
-
+    
     producto.cantidad--;
     cantidadProductos--;
-
+    
     if(producto.cantidad == 0){
-       carrito = carrito.filter(prod => prod !== producto);
+        carrito = carrito.filter(prod => prod !== producto);
     }
     
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-    localStorage.setItem("cantidadProductos", JSON.stringify(cantidadProductos));
+    // localStorage.setItem("carrito", JSON.stringify(carrito));
+    // localStorage.setItem("cantidadProductos", JSON.stringify(cantidadProductos));
+    
+    actualizarLocalStorage(carrito, cantidadProductos);
+    actualizarCarrito();
+    cargarCarrito();
+}
 
+function eliminarProducto(carrito, producto){
+    let cantidadProductos = Number (localStorage.getItem("cantidadProductos"));
+    cantidadProductos -= producto.cantidad;
+    carrito = carrito.filter(prod => prod !== producto);
+    
+    actualizarLocalStorage(carrito, cantidadProductos);
     actualizarCarrito();
     cargarCarrito();
 }
